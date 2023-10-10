4f56a9ea719506069bb1dde55fa2dc8d7b7c7574
erdaer <147092818+erdaer@users.noreply.github.com>
2023-10-10 10:14:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,147 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import numpy as np
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+import matplotlib.pyplot as plt
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+ 
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-st.print('ett test')
+def compute_q_ensemble(K, H, r, psi):
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+    # Compute q for each set of parameters
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    q_ensemble = (2 * np.pi * K * H) / (np.log(2 * H / r) + psi)
 
-    points_per_turn = total_points / num_turns
+    return q_ensemble
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+ 
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+def compute_q_inj_ensemble(K, H, r, psi, K_inj_factor, tau):
+
+    # Compute K_inj as a ratio between K and K_inj_factor
+
+    K_inj = K / K_inj_factor
+
+ 
+
+    # Compute q_inj for each set of parameters
+
+    q_inj_ensemble = (2 * np.pi * K * H) / (np.log(2 * H / r) + ((K / K_inj - 1) * np.log(1 + tau / r)) + psi)
+
+    return q_inj_ensemble
+
+ 
+
+# Use Streamlit widgets to get user input
+
+n_samples = st.sidebar.slider('Number of samples', 1000, 10000, 5000)
+
+ 
+
+# Define the distribution type for each parameter
+
+distribution_type = {
+
+    'K': st.sidebar.selectbox('Distribution type for K', ['uniform', 'normal']),
+
+    'H': st.sidebar.selectbox('Distribution type for H', ['uniform', 'normal']),
+
+    'r': st.sidebar.selectbox('Distribution type for r', ['uniform', 'normal']),
+
+    'psi': st.sidebar.selectbox('Distribution type for psi', ['uniform', 'normal']),
+
+    'K_inj_factor': st.sidebar.selectbox('Distribution type for K_inj_factor', ['uniform', 'normal']),
+
+    'tau': st.sidebar.selectbox('Distribution type for tau', ['uniform', 'normal'])
+
+}
+
+ 
+
+# Define the bounds or mean and std dev for each parameter
+
+param_values = {}
+
+for param in distribution_type.keys():
+
+    if distribution_type[param] == 'uniform':
+
+        min_value = st.slider(f'Enter min value for {param}',1,100,2)
+
+        max_value = st.text_input(f'Enter max value for {param}')
+
+        param_values[param] = (float(min_value), float(max_value))
+
+    elif distribution_type[param] == 'normal':
+
+        mean_value = st.text_input(f'Enter mean value for {param}')
+
+        stddev_value = st.text_input(f'Enter std dev value for {param}')
+
+        param_values[param] = (float(mean_value), float(stddev_value))
+
+ 
+
+# Generate random samples for K, H, r, psi, K_inj_factor and tau
+
+parameters = ['K', 'H', 'r', 'psi', 'K_inj_factor', 'tau']
+
+samples = {}
+
+for param in parameters:
+
+    if distribution_type[param] == 'uniform':
+
+        samples[param] = np.random.uniform(low=param_values[param][0], high=param_values[param][1], size=n_samples)
+
+    elif distribution_type[param] == 'normal':
+
+        samples[param] = np.random.normal(loc=param_values[param][0], scale=param_values[param][1], size=n_samples)
+
+ 
+
+# Call the function with the generated samples
+
+q_ensemble = compute_q_ensemble(samples['K'], samples['H'], samples['r'], samples['psi'])
+
+q_inj_ensemble = compute_q_inj_ensemble(samples['K'], samples['H'], samples['r'], samples['psi'], samples['K_inj_factor'], samples['tau'])
+
+ 
+
+# Create a new figure
+
+plt.figure()
+
+ 
+
+# Plot histogram for q_ensemble
+
+plt.hist(q_ensemble, bins=50, alpha=0.5, label='q')
+
+ 
+
+# Plot histogram for q_inj_ensemble
+
+plt.hist(q_inj_ensemble, bins=50, alpha=0.5, label='q_inj')
+
+ 
+
+# Add title and labels
+
+plt.title('Histograms for q and q_inj')
+
+plt.xlabel('Value')
+
+plt.ylabel('Frequency')
+
+ 
+
+# Add legend
+
+plt.legend(loc='upper right')
+
+ 
+
+# Display the plot in Streamlit
+
+st.pyplot(plt)
